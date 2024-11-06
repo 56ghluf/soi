f64b72fba22623da020a14b0c00486af9e9b003e
famemonkey <redbirdhasflown@gmail.com>
2024-11-06 16:57:34 +0100

    main: wtf is going on?

--- land/main.cpp
+++ land/main.cpp
@@ -65,6 +65,7 @@ std::vector<std::tuple<int, int>>& slice_with_indicies(int start, int end, int* 
 
     for (int i = start; i < end; i++) {
         slice[i] = std::make_tuple(line_data[i], i-start);
+        std::cout << line_data[i] << " " << i-start << std::endl;
     }
 
     return slice;
@@ -75,7 +76,7 @@ bool sort_desc(const std::tuple<int, int>& a, const std::tuple<int, int>& b) {
 }
 
 int main() {
-    input_data data = load_input_data("input1.txt");
+    input_data data = load_input_data("input.txt");
 
     int lines = data.lines;
     int* line_sizes = data.line_sizes;
@@ -147,21 +148,27 @@ int main() {
     int height;
     int current_pos;
 
-    int max_length;
+    int max_height;
+    int max_valid_height;
 
     cumulated_line_sizes = 0;
     current_line_size = 0;
     for (int i = 0; i < lines; i++) {
-        if (i != 0) {
-            break;
-        }
-
         cumulated_line_sizes += current_line_size;
         current_line_size = line_sizes[i];
 
-        max_length = 0;
+        if (i != 1) {
+            continue;
+        }
+
+        max_height = 0;
+        max_valid_height = 0;
 
         std::vector<std::tuple<int, int>>& slice = slice_with_indicies(cumulated_line_sizes, cumulated_line_sizes+current_line_size, line_data);
+        std::sort(slice.begin(), slice.end(), sort_desc);
+        for (int j = 0; j < current_line_size; j++) {
+            std::cout << std::get<0>(slice[j]) << " " << std::get<1>(slice[j]) << std::endl;
+        }
 
         for (int j = 0; j < current_line_size; j++) {
             height = std::get<0>(slice[j]);
@@ -169,7 +176,13 @@ int main() {
             distance_left = current_line_size - 1 - index;
 
             if (index >= height-1 && distance_left >= height-1) {
-                std::cout << "Range worked for index: " << index << std::endl;
+                if (height < max_valid_height) {
+                    break;
+                }
+                //if (j != 2) {
+                //    continue;
+                //}
+
                 current_pos = cumulated_line_sizes + index - height + 1;
 
                 if (line_data[current_pos] != 1 || line_data[cumulated_line_sizes+index+height-1] != 1) {
@@ -180,29 +193,40 @@ int main() {
 
                 for (; current_pos < cumulated_line_sizes + index + height-1; current_pos++) {
                     if (before_max) {
-                        if (line_data[current_pos+1] - line_data[current_pos] != 1) {
-                            goto broke;
+                        if (line_data[current_pos+1] - line_data[current_pos] == 1) {
+                            continue;
                         }
 
                         if (line_data[current_pos+1] - line_data[current_pos] == -1) {
                             before_max = false;
+                            continue;
                         }
+
+                        goto broke;
                     }
                     if (!before_max) {
-                        if (line_data[current_pos+1] - line_data[current_pos] != -1) {
-                            goto broke;
+                        if (line_data[current_pos+1] - line_data[current_pos] == -1) {
+                            continue;
                         }               
-                    }
-               }
 
-                if (2*height - 1 > max_length) max_length = 2*height - 1;
+                        goto broke;
+                    }
+                }
+
+                if (height > max_valid_height) {
+                    max_valid_height = height;
+                } 
+
+                if (2*height - 1 > max_height) {
+                    max_height = 2*height - 1;
+                }
 
                 broke:
+                continue;
             }
-
         }
+        std::cout << "Case #" << i << ": " << max_height << std::endl;
 
-        std::cout << "Case #" << i << ": " << max_length << std::endl;
     }
     return 0;
 }
